e43a269d257e500dff853095bd4fc8d856d1c363
dinhwe2612 <116061849+dinhwe2612@users.noreply.github.com>
2023-06-27 14:25:44 +0700

    Add files via upload

---
+++ External/raylib/out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: E:/HCMUS/cs163/CS163-Project/External/raylib
+# Build directory: E:/HCMUS/cs163/CS163-Project/External/raylib/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("raylib")
+subdirs("examples")
---
+++ External/raylib/out/build/x64-Debug/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: E:/HCMUS/cs163/CS163-Project/External/raylib/src
+# Build directory: E:/HCMUS/cs163/CS163-Project/External/raylib/out/build/x64-Debug/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("external/glfw")
